695f92d336528a625287aabf00cab37e221a630c
최지연 <jyc@joonggonara.co.kr>
2022-10-19 16:52:08 +0900

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Jiyeon Choi on 2022. 10. 19..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
